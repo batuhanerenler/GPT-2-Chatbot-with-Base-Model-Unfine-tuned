54e16c08cec0a2c5c8c045ecd1e4458e4bb11d7a
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-03-16 15:02:19 +0300

    Update and rename fine-tuner-tpu.py to fine-tuner-gpu-colab.py
    Now you can fine tune your model with Colab GPU's. Have fun, enjoy

--- fine-tuner-gpu-colab.py
+++ fine-tuner-gpu-colab.py
@@ -3,10 +3,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
 from datasets import load_dataset, Dataset
+import torch
+from google.colab import drive
+from google.colab import files
+import shutil
+import matplotlib.pyplot as plt
+
+# Set the device to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelForCausalLM.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("gpt2").to(device)
+
+# Calculate the number of trainable parameters before training
+num_params_before = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Number of trainable parameters before training: {num_params_before}")
 
 # Set the pad_token to the eos_token
 tokenizer.pad_token = tokenizer.eos_token
@@ -25,7 +37,6 @@ def tokenize_function(examples):
 train_dataset = train_dataset.map(tokenize_function, batched=True)
 val_dataset = val_dataset.map(tokenize_function, batched=True)
 
-
 # Create a data collator
 from transformers import DataCollatorForLanguageModeling
 
@@ -42,7 +53,7 @@ training_args = TrainingArguments(
     save_strategy="epoch",
     logging_dir="./logs",
     logging_steps=200,
-    
+    fp16=True,  # Enable mixed precision training
 )
 
 # Create the Trainer
@@ -56,3 +67,25 @@ trainer = Trainer(
 
 # Train the model
 trainer.train()
+
+# Calculate the number of trainable parameters after training
+num_params_after = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Number of trainable parameters after training: {num_params_after}")
+
+# Save the trained model to Google Drive
+drive.mount('/content/drive')
+model_save_path = "/content/drive/MyDrive/chatbot_model"
+trainer.save_model(model_save_path)
+
+# Zip the saved model folder
+shutil.make_archive("/content/drive/MyDrive/chatbot_model", 'zip', model_save_path)
+
+# Download the zipped folder to your computer
+files.download("/content/drive/MyDrive/chatbot_model.zip")
+
+# Plot the training loss
+plt.plot(trainer.state.log_history)
+plt.xlabel("Training Steps")
+plt.ylabel("Loss")
+plt.title("Training Loss")
+plt.show()
